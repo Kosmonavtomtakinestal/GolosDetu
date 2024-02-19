f9ec41dabf4c52773710b5b1de7111e5d8a36f47
Ruslan <Ruslan@DESKTOP-NR8DVE4>
2024-02-19 13:50:08 +0300

    отредактировал регистрацию и авторизацию, добавил окно просмотра кандидата, добавил обработчик двойного клика по списку кандидатов

---
+++ LSDA/LSDA/View/ShowParticipantWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using LSDA.Database.Models;
+using System.Windows;
+
+namespace LSDA.View
+{
+    public partial class ShowParticipantWindow : Window
+    {
+        public Participant CurrentParticipant { get; set; }
+
+        public ShowParticipantWindow(Participant participant)
+        {
+            CurrentParticipant = participant;
+
+            InitializeComponent();
+        }
+    }
+}
